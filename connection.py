2ff2e66cf209357d652acdd7552cefa1acaab1a1
Jesuloba David <Jesulobadavid@gmail.com>
2022-11-30 23:01:15 -0600

    2nd updates

---
+++ connection.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import shortuuid
+
+class login_table:
+    def __init__(self):
+        self.host = "localhost"
+        self.database = "marketPlace"
+        self.user = "root"
+        self.password = "loba"
+        self.data = mysql.connector.connect(self.host, self.database, self.user, self.password)
+        
+
+    def sign_up(self, username, passwd, email):
+        try:
+            
+
+            # username = input("Enter Username: ")
+            # passwd = input("Enter Password: ")
+            # Email = input("Enter Email: ")
+            uid = shortuuid.ShortUUID().random(length=15)
+            print(uid)
+
+            mycursor = self.data.cursor()
+
+            sql = "INSERT INTO login_table (username, passwd, email, user_id) VALUES (%s, %s, %s, %s)"
+            val = (username, passwd, email, uid)
+            mycursor.execute(sql, val)
+
+            self.data.commit()
+
+            print(mycursor.rowcount, "record inserted.")
+
+        except mysql.connector.Error as error:
+            print("Failed to insert record into Laptop table {}".format(error))
+    
+    def log_in(self, email, passwd):
+        sql = "SELECT user_id FROM login_table WHERE email = $ AND passwd = $"
+        self.data
+        pass
--- first.py
+++ first.py
@@ -2,50 +2,78 @@ from flask import Flask
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
-import mysql.connector
-import shortuuid
+from connection import login_table
 
 
 auth = Blueprint('auth', __name__)
 auth = Flask(__name__)
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        print(email)
+        password = request.form.get('password1')
+        print(password)
+        db = login_table()
+        db.log_in(email, password)
+
+    #     user = db.query.filter_by(email=email).first()
+    #     if not user:
+    #         flash('Email does not exist.', category='error')
+    #         return render_template('login.html') 
+    #         # user=current_user
+    #     if not check_password_hash(user.password, password):
+    #         flash('Incorrect password, try again.', category='error')
+    #         return render_template('login.html') 
+        
+    #     flash('Logged in successfully!', category='success')
+    #     login_user(user, remember=True)
+    #     return render_template('home.html')
+    
+    return render_template('login.html')
+        # else:
+        #     flash('Incorrect password, try again.', category='error')
+        # # else:
+        #     flash('Email does not exist.', category='error')
+
+    # return render_template('login.html', user=current_user)
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    # if request.method == 'POST':
+    #     email = request.form.get('email')
+    #     user_name = request.form.get('userName')
+    #     password1 = request.form.get('password1')
+    #     password2 = request.form.get('password2')
+
+    #     if len(email) < 4:
+    #         flash('Email must be greater than 3 characters.', category='error')
+    #     elif len(user_name) < 2:
+    #         flash('Username must be greater than 1 character.', category='error')
+    #     elif password1 != password2:
+    #         flash('Passwords don\'t match.', category='error')
+    #     else:
+    #         new_user = User(email=email, user_name=user_name,
+    #                         password=generate_password_hash(password1, method='pbkdf2:sha1', salt_length=8))
+    #         db.session.add(new_user)
+    #         login_user(new_user, remember=True)
+    #         flash('Account created!', category='success')
+    #         db.session.commit()
+    #         return (redirect(url_for('auth.login')))
+
+    return render_template('signup.html')
+
 @auth.route("/")
 @auth.route('/landing')
 def landing():
-    return render_template('index.html')
+    return render_template('products.html')
 
 @auth.route('/product')
 def product():
     return render_template('products.html')
 
-'''
-try:
-    data = mysql.connector.connect(
-    host="localhost",
-    database='marketPlace',
-    user="root",
-    password="loba"
-    )
-    print(data)
 
-    username = input("Enter Username: ")
-    passwd = input("Enter Password: ")
-    Email = input("Enter Email: ")
-    uid = shortuuid.ShortUUID().random(length=15)
-    print(uid)
-
-    mycursor = data.cursor()
-
-    sql = "INSERT INTO login_table (username, passwd, email, user_id) VALUES (%s, %s, %s, %s)"
-    val = (username, passwd, Email, uid)
-    mycursor.execute(sql, val)
-
-    data.commit()
-
-    print(mycursor.rowcount, "record inserted.")
-
-except mysql.connector.Error as error:
-    print("Failed to insert record into Laptop table {}".format(error))
-'''
 if __name__ == "__main__":
     auth.run(debug=True)
